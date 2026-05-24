ddce1f158af2e76300f0525bd1b0fc7976595e36
PUSPENDU GHOSH <122379716+PuspenduGitHub@users.noreply.github.com>
2026-05-25 03:56:18 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-
 import requests
 import os
 
@@ -32,7 +31,7 @@ def analyze_soil(data: SoilInput):
     ...
     """
 
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 
     response = requests.post(
         url,
